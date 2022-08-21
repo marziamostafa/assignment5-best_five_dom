4220d7e70425b06b904bd87e9c56a74cf9db4139
marziamostafa <marziamostafa@gmail.com>
2022-08-22 02:26:43 +0600

    third push

---
+++ js/budget.js
@@ -0,0 +1,13 @@
+console.log('button');
+
+
+
+document.getElementById('btn-player-budget-calculate').addEventListener('click', function () {
+
+
+
+})
+
+document.getElementById('btn-total-budget-calculate').addEventListener('click', function () {
+
+})
